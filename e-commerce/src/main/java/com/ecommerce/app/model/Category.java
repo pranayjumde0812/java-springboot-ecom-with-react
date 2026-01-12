d550d8eaa6e9c888cb97c6d8cf77f9cfe47d7074
Pranay Jumde <pranayjumde13@gmail.com>
2026-01-13 00:27:37 +0530

    validation

--- e-commerce/src/main/java/com/ecommerce/app/model/Category.java
+++ e-commerce/src/main/java/com/ecommerce/app/model/Category.java
@@ -2,6 +2,7 @@ package com.ecommerce.app.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,5 +19,6 @@ public class Category {
     private Long categoryId;
 
     @NotBlank
+    @Size(min = 5, message = "Category Name Must contain at least 5 characters")
     private String categoryName;
 }
