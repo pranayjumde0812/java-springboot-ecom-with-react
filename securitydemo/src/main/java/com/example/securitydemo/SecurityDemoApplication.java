3d4fa97a682c28a8e27cb27013e2b8ff0dbaa8e6
Pranay Jumde <pranayjumde13@gmail.com>
2026-01-19 01:21:13 +0530

    updated

--- securitydemo/src/main/java/com/example/securitydemo/SecurityDemoApplication.java
+++ securitydemo/src/main/java/com/example/securitydemo/SecurityDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecuritydemoApplication {
+public class SecurityDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecuritydemoApplication.class, args);
+		SpringApplication.run(SecurityDemoApplication.class, args);
 	}
 
 }
