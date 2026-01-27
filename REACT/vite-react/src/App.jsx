3011cc4b411f61d498b3076ff7305c6cd5a653ae
Pranay Jumde <pranay.jumde@qodequay.com>
2026-01-27 12:45:50 +0530

    added array in props

--- REACT/vite-react/src/App.jsx
+++ REACT/vite-react/src/App.jsx
@@ -4,14 +4,16 @@ import "./App.css";
 import ProfileCard from "./components/ProfileCard";
 
 function App() {
+
+  const hobbies1 = ["Reading", "Traveling", 1];
   return (
     <>
       <div>
         <h1>Hello</h1>
         {/* <MyButton /> */}
 
-        <ProfileCard name="Pranay" age={24} isMember={true} />
-        <ProfileCard name="Rahul" age={30} isMember={false} />
+        <ProfileCard name="Pranay" age={24} isMember={true} hobbies={hobbies1}/>
+        <ProfileCard name="Rahul" age={30} isMember={false} hobbies={hobbies1}/>
       </div>
     </>
   );
--- REACT/vite-react/src/components/ProfileCard.jsx
+++ REACT/vite-react/src/components/ProfileCard.jsx
@@ -1,12 +1,18 @@
 import PropTypes from "prop-types";
 
-function ProfileCard({ name, age, isMember }) {
+function ProfileCard({ name, age, isMember, hobbies }) {
   return (
     <>
       <div>
         <h3>Name: {name}</h3>
         <h3>Age: {age}</h3>
         <h3>Status: {isMember ? "Active Member" : "Guest"}</h3>
+        <h3>Hobbies:</h3>
+        <ul>
+          {hobbies.map((hobby, index) => {
+            return <li key={index + 1}>{hobby}</li>;
+          })}
+        </ul>
       </div>
     </>
   );
@@ -16,6 +22,7 @@ ProfileCard.propTypes = {
   name: PropTypes.string,
   age: PropTypes.number,
   isMember: PropTypes.bool.isRequired,
+  hobbies: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default ProfileCard;
