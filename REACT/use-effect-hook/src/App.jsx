9d8ddbc5d2c6633c25165679bb5e44a918b1061a
Pranay Jumde <pranayjumde13@gmail.com>
2026-01-29 01:26:17 +0530

    use of useEffect

--- REACT/use-effect-hook/src/App.jsx
+++ REACT/use-effect-hook/src/App.jsx
@@ -5,8 +5,23 @@ function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
+    // When we pass dependecy like here [count] then only when changes happen in
+    // count then this code will run. everytime when count changes
+
     document.title = `Count : ${count}`;
+    console.log("Use Effect is Triggered");
   }, [count]);
+
+  // useEffect(() => {
+  //   // Use Only when have to perform some task  at time of component mount
+  //   // empty [] passing only runs this at the time of component mounting
+  //   document.title = `Count : ${count}`;
+  //   console.log("Use Effect is Triggered");
+  // }, []);
+
+  // useEffect(() => {
+  //   // Run every time when component render , anything changes then this code will run
+  // });
 
   const incrementCount = () => {
     setCount(count + 1);
