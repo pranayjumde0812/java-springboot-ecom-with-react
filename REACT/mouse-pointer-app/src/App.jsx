6a7c3f7ff8087496ccddbae94adfd6357ca9ee50
Pranay Jumde <pranayjumde13@gmail.com>
2026-01-29 01:47:05 +0530

    mouse postion app done

--- REACT/mouse-pointer-app/src/App.jsx
+++ REACT/mouse-pointer-app/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+  }, []);
 
   return (
     <>
-      <div></div>
+      <div>
+        <h2>Mouse Position</h2>
+        <p>
+          X: {mousePosition.x}, Y: {mousePosition.y}
+        </p>
+      </div>
     </>
   );
 }
