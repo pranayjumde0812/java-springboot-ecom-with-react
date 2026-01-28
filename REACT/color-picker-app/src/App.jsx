51152c80d89136553e0dd8a3bd2c10ef7a9c22c1
Pranay Jumde <pranay.jumde@qodequay.com>
2026-01-28 15:50:57 +0530

    created color picker application

---
+++ REACT/color-picker-app/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [backgroundColor, setBackgroundColor] = useState("#ffffff");
+
+  const colors = [
+    "#ff0000",
+    "#00ff00",
+    "#0000ff",
+    "#ffff00",
+    "#ff00ff",
+    "#00ffff",
+  ];
+
+  const handleColorChange = (color) => {
+    setBackgroundColor(color);
+  };
+
+  return (
+    <>
+      <div className="App" style={{ backgroundColor }}>
+        <h1>Color Picker</h1>
+        <div className="color-palette">
+          {colors.map((color, index) => (
+            <div
+              key={index}
+              className="color-box"
+              style={{ backgroundColor: color }}
+              onClick={() => handleColorChange(color)}
+            ></div>
+          ))}
+        </div>
+
+        {/* Select Random Colors */}
+
+        <div className="custom-color-picker">
+          <input
+            type="color"
+            value={backgroundColor}
+            onChange={(e) => handleColorChange(e.target.value)}
+          />
+        </div>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
