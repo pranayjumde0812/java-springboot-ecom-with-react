6485dbd382eca4724797956bf6d093d1464e4423
Pranay Jumde <pranay.jumde@qodequay.com>
2026-01-28 12:39:10 +0530

    passing array and object to state

--- REACT/vite-react/src/App.jsx
+++ REACT/vite-react/src/App.jsx
@@ -42,53 +42,95 @@ import InputBox from "./components/InputBox";
 // }
 
 // Hooks Example
+
+////// Counter App //////
+
+// function App() {
+//   // useState
+//   const [count, setCount] = useState(0);
+//   const [step, setStep] = useState(1);
+
+//   const incrementCount = () => setCount(count + step);
+
+//   const decrementCount = () => setCount(count - step);
+
+//   const stepChangeHandler = (e) => {
+//     setStep(parseInt(e.target.value || "0"));
+//   };
+
+//   const resetCounter = () => {
+//     setCount(0);
+//     setStep(1);
+//   };
+
+//   return (
+//     <>
+//       <div className="app-container">
+//         <h1>Counter App</h1>
+//         <h2>Count: {count}</h2>
+//         {/* <button onClick={() => setCount(count + 1)}>Increment</button>
+//         <button onClick={() => setCount(count - 1)}>Decrement</button> */}
+//         <label>Step:</label>
+
+//         {/* <input
+//           type="number"
+//           value={step}
+//           onChange={(e) => setStep(parseInt(e.target.value || "0"))}
+//         /> */}
+
+//         <InputBox
+//           type="number"
+//           placeholder="Enter step"
+//           value={step}
+//           onChange={stepChangeHandler}
+//         />
+
+//         <br />
+//         <MyButton actionName={"Increment"} onClick={incrementCount} />
+//         <MyButton actionName={"Decrement"} onClick={decrementCount} />
+//         <MyButton actionName={"Reset"} onClick={resetCounter} />
+//       </div>
+//     </>
+//   );
+// }
+
+////// Passing Array and Object to State //////
+
 function App() {
-  // useState
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  const [counters, setCounters] = useState([{ id: 1, value: 0 }]);
 
-  const incrementCount = () => setCount(count + step);
-
-  const decrementCount = () => setCount(count - step);
-
-  const stepChangeHandler = (e) => {
-    setStep(parseInt(e.target.value || "0"));
+  const addCounters = () => {
+    setCounters([...counters, { id: counters.length + 1, value: 0 }]);
   };
 
-  const resetCounter = () => {
-    setCount(0);
-    setStep(1);
+  const incrementCounter = (id) => {
+    setCounters(
+      counters.map((counter) =>
+        counter.id === id ? { ...counter, value: counter.value + 1 } : counter,
+      ),
+    );
   };
 
   return (
     <>
       <div className="app-container">
-        <h1>Counter App</h1>
-        <h2>Count: {count}</h2>
-        {/* <button onClick={() => setCount(count + 1)}>Increment</button>
-        <button onClick={() => setCount(count - 1)}>Decrement</button> */}
-        <label>Step:</label>
-
-        {/* <input
-          type="number"
-          value={step}
-          onChange={(e) => setStep(parseInt(e.target.value || "0"))}
-        /> */}
-
-        <InputBox
-          type="number"
-          placeholder="Enter step"
-          value={step}
-          onChange={stepChangeHandler}
-        />
-
-        <br />
-        <MyButton actionName={"Increment"} onClick={incrementCount} />
-        <MyButton actionName={"Decrement"} onClick={decrementCount} />
-        <MyButton actionName={"Reset"} onClick={resetCounter} />
+        <button onClick={addCounters}>Add Counter</button>
+        <ul>
+          {counters.map((counter) => (
+            <li key={counter.id}>
+              Counter {counter.id}: {counter.value}
+              <button
+                onClick={() => {
+                  incrementCounter(counter.id);
+                }}
+              >
+                Increment
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
 }
-
 export default App;
