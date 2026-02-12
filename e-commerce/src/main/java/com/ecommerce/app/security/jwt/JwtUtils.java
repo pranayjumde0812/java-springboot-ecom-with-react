f11a202f3890ad5981fef609c1835e7db66805ea
Pranay Jumde <pranayjumde13@gmail.com>
2026-02-13 01:41:33 +0530

    signout done

--- e-commerce/src/main/java/com/ecommerce/app/security/jwt/JwtUtils.java
+++ e-commerce/src/main/java/com/ecommerce/app/security/jwt/JwtUtils.java
@@ -116,4 +116,12 @@ public class JwtUtils {
 
         return cookie;
     }
+
+    public ResponseCookie getCleanJwtCookie() {
+        ResponseCookie cookie = ResponseCookie.from(jwtCookie, null)
+                .path("/api")
+                .build();
+
+        return cookie;
+    }
 }
