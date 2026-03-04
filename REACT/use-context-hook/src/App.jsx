b20c414e97ea4c493bc9407ea8bf6c70f0dc4b39
Pranay Jumde <pranay.jumde@qodequay.com>
2026-03-05 02:45:46 +0530

    create and use context

--- REACT/use-context-hook/src/App.jsx
+++ REACT/use-context-hook/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { createContext, useContext, useState } from 'react'
 import './App.css'
 
 // - App = DATA
@@ -6,42 +6,55 @@ import './App.css'
 //     - ComponentB
 //       - ThemedComponent =  needs theme prop
 
+// useContext()
+// 1. Create Context
+//    const ThemeContext = createContext(defaultValue);
+// 2. Provide Context
+//    <ThemeContext.Provider value={value}>
+//      ...
+//    </ThemeContext.Provider>
+// 3. Consume Context
+//    const value = useContext(ThemeContext);
+
+const ThemeContext = createContext("light");
+
 function App() {
-  const theme = "dark";
+  // const theme = "dark";
 
   return (
     <>
       <div className="app" style={{ border: '2px solid black', padding: "20px", margin: "20px", textAlign: "center" }}>
         <h2>App(parent) Component</h2>
-        <ComponentA theme={theme} />
+        <ComponentA />
       </div>
     </>
   )
 }
 
-function ComponentA({ theme }) {
+function ComponentA() {
   return (
     <>
       <div className="app" style={{ border: '2px solid blue', padding: "20px", margin: "20px" }}>
         <h2>ComponentA(child) Component</h2>
-        <ComponentB theme={theme} />
+        <ComponentB />
       </div>
     </>
   )
 }
 
-function ComponentB({ theme }) {
+function ComponentB() {
   return (
     <>
       <div className="app" style={{ border: '2px solid green', padding: "20px", margin: "20px" }}>
         <h2>ComponentB(grandchild) Component</h2>
-        <ThemedComponent theme={theme} />
+        <ThemedComponent />
       </div>
     </>
   )
 }
 
-function ThemedComponent({ theme }) {
+function ThemedComponent() {
+  const theme = useContext(ThemeContext);
   return (
     <>
       <div className="app" style={{ border: '2px solid red', padding: "20px", margin: "20px" }}>
