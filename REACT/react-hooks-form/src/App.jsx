f3a8219a99705dee0c89691543782b94da519063
Pranay Jumde <pranay.jumde@qodequay.com>
2026-03-10 03:12:41 +0530

    feat: implement a basic React form with state management for name, email, and password inputs, and add foundational styling.

--- REACT/react-hooks-form/src/App.jsx
+++ REACT/react-hooks-form/src/App.jsx
@@ -1,9 +1,58 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  }
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
   return (
     <>
-      <h1>React Hooks Form</h1>
+      <div>
+        <h1>React Hooks Form</h1>
+        <form onSubmit={handleSubmit}>
+          <label>Name :
+            <input
+              type="text"
+              id="name"
+              name="name"
+              onChange={handleChange} /> <br />
+          </label>
+
+          <label>Email :
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={handleChange} /> <br />
+          </label>
+
+          <label>Password :
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={handleChange} /> <br />
+          </label>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </>
   )
 }
