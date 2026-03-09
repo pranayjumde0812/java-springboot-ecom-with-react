5262002877c017a2977d8b336d53d2f89c5ad49a
Pranay Jumde <pranay.jumde@qodequay.com>
2026-03-09 12:15:51 +0530

    feat: Fetch and display both posts and users data concurrently using `axios.all` instead of only posts.

--- REACT/working-with-apis/src/App.jsx
+++ REACT/working-with-apis/src/App.jsx
@@ -4,17 +4,24 @@ import './App.css'
 
 function App() {
 
-  const [data, setData] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     setLoading(true);
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(response => {
-        setData(response.data);
+    axios.all([
+      axios.get('https://jsonplaceholder.typicode.com/posts'),
+      axios.get('https://jsonplaceholder.typicode.com/users')
+    ])
+      .then(axios.spread((posts, users) => {
+        console.log(posts);
+        console.log(users);
+        setPosts(posts.data);
+        setUsers(users.data);
         setLoading(false);
-      })
+      }))
       .catch(error => {
         setError(error);
         setLoading(false);
@@ -34,11 +41,21 @@ function App() {
       <div>
         <h1>Data from API</h1>
         <ul>
-          {data.map(item => (
+          {posts.map(item => (
             <li key={item.id}>
               <h2>{item.title}</h2>
               <p>{item.body}</p>
               <p>{item.userId}</p>
+              <p>{item.id}</p>
+              <hr />
+            </li>
+          ))}
+        </ul>
+        <ul>
+          {users.map(item => (
+            <li key={item.id}>
+              <h2>{item.name}</h2>
+              <p>{item.email}</p>
               <p>{item.id}</p>
               <hr />
             </li>
