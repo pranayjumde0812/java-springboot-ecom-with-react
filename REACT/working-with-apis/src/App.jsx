96deaa127e6133e00462f84e6010f5ca059a6188
Pranay Jumde <pranay.jumde@qodequay.com>
2026-03-09 15:21:35 +0530

    feat: Add a form for creating and submitting new posts and comment out the previous data fetching and display logic.

--- REACT/working-with-apis/src/App.jsx
+++ REACT/working-with-apis/src/App.jsx
@@ -4,66 +4,102 @@ import './App.css'
 
 function App() {
 
-  const [posts, setPosts] = useState([]);
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  //   const [posts, setPosts] = useState([]);
+  //   const [users, setUsers] = useState([]);
+  //   const [loading, setLoading] = useState(false);
+  //   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    setLoading(true);
-    axios.all([
-      axios.get('https://jsonplaceholder.typicode.com/posts'),
-      axios.get('https://jsonplaceholder.typicode.com/users')
-    ])
-      .then(axios.spread((posts, users) => {
-        console.log(posts);
-        console.log(users);
-        setPosts(posts.data);
-        setUsers(users.data);
-        setLoading(false);
-      }))
-      .catch(error => {
-        setError(error);
-        setLoading(false);
+  //   useEffect(() => {
+  //     setLoading(true);
+  //     axios.all([
+  //       axios.get('https://jsonplaceholder.typicode.com/posts'),
+  //       axios.get('https://jsonplaceholder.typicode.com/users')
+  //     ])
+  //       .then(axios.spread((posts, users) => {
+  //         console.log(posts);
+  //         console.log(users);
+  //         setPosts(posts.data);
+  //         setUsers(users.data);
+  //         setLoading(false);
+  //       }))
+  //       .catch(error => {
+  //         setError(error);
+  //         setLoading(false);
+  //       })
+  //   }, []);
+
+  //   if (loading) {
+  //     return <div><p>Loading...</p></div>;
+  //   }
+
+  //   if (error) {
+  //     return <div><p>Error fetching data: {error.message}</p></div>;
+  //   }
+
+  //   return (
+  //     <>
+  //       <div>
+  //         <h1>Data from API</h1>
+  //         <ul>
+  //           {posts.map(item => (
+  //             <li key={item.id}>
+  //               <h2>{item.title}</h2>
+  //               <p>{item.body}</p>
+  //               <p>{item.userId}</p>
+  //               <p>{item.id}</p>
+  //               <hr />
+  //             </li>
+  //           ))}
+  //         </ul>
+  //         <ul>
+  //           {users.map(item => (
+  //             <li key={item.id}>
+  //               <h2>{item.name}</h2>
+  //               <p>{item.email}</p>
+  //               <p>{item.id}</p>
+  //               <hr />
+  //             </li>
+  //           ))}
+  //         </ul>
+  //       </div>
+  //     </>
+  //   )
+
+
+  // Post 
+  const [data, setData] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newPost = {
+      title: e.target.title.value,
+      body: e.target.body.value,
+      userId: 1
+    }
+    axios.post('https://jsonplaceholder.typicode.com/posts', newPost)
+      .then(response => {
+        console.log('new post added ', response.data);
+        setData([...data, response.data]);
       })
-  }, []);
-
-  if (loading) {
-    return <div><p>Loading...</p></div>;
-  }
-
-  if (error) {
-    return <div><p>Error fetching data: {error.message}</p></div>;
+      .catch(error => {
+        console.log(error);
+      })
   }
 
   return (
     <>
       <div>
-        <h1>Data from API</h1>
-        <ul>
-          {posts.map(item => (
-            <li key={item.id}>
-              <h2>{item.title}</h2>
-              <p>{item.body}</p>
-              <p>{item.userId}</p>
-              <p>{item.id}</p>
-              <hr />
-            </li>
-          ))}
-        </ul>
-        <ul>
-          {users.map(item => (
-            <li key={item.id}>
-              <h2>{item.name}</h2>
-              <p>{item.email}</p>
-              <p>{item.id}</p>
-              <hr />
-            </li>
-          ))}
-        </ul>
+        <h1>Add Post</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="title" placeholder='Title' /> <br />
+          <input type="text" name="body" placeholder='Body' /> <br />
+
+          <button type='submit'>Add Post</button>
+        </form>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
